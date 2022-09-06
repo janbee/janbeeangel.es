1519a7eef9543b54f6782f8ab6e69eb186e85a70
Janbets <janbeeangeles@gmail.com>
2022-09-06 14:18:00 -0600

    no message

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -2,7 +2,7 @@ import { ajax } from 'rxjs/internal/ajax/ajax';
 import { Config } from '@models/env';
 import { map } from 'rxjs';
 import { DataModel } from '@models/custom.models';
-import { camelCase, find } from 'lodash';
+import { camelCase, find, get } from 'lodash';
 
 class ApiService {
   getDataJson() {
@@ -17,24 +17,22 @@ class ApiService {
 
         // @ts-ignore
         res.sheets
-          // @ts-ignore
-          .filter((item) => item.properties.title !== 'Name / Intro')
-          // @ts-ignore
-          .forEach((item) => {
-            const key = camelCase(item.properties.title);
+          .filter((item: { properties: { title: string } }) => item.properties.title !== 'Name / Intro')
+          .forEach((item: unknown) => {
+            const key = camelCase(get(item, 'properties.title'));
+            const rows = get(item, 'data[0].rowData');
 
-            dataModel[key] = [];
-            // @ts-ignore
-            item.data[0].rowData.forEach((row, rowIndex) => {
-              if (rowIndex !== 0) {
-                const obj = {};
-                // @ts-ignore
-                row.values.forEach((val, valIndex) => {
-                  obj[item.data[0].rowData[0].values[valIndex].formattedValue] = val.formattedValue;
-                });
+            const dataKeys = get(rows.shift(), 'values').map(
+              ({ formattedValue }: { formattedValue: string }) => formattedValue
+            );
 
-                dataModel[key].push(obj);
-              }
+            dataModel[key] = rows.map((row: { values: { formattedValue: unknown }[] }) => {
+              return dataKeys.reduce((prev: { [x: string]: unknown }, dataKey: string, index: number) => {
+                console.log(prev, dataKey, index);
+
+                prev[dataKey] = row.values[index].formattedValue;
+                return prev;
+              }, {});
             });
           });
 
